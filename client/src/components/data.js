fa371f147f57e5447aea1cccd1ede5964477f4b7
Venkateshcoder2003 <venkateshvenki77644@gmail.com>
2024-10-24 20:38:16 +0530

    added another project

--- client/src/components/data.js
+++ client/src/components/data.js
@@ -13,6 +13,13 @@ const projectData = [
         github: "https://github.com/Venkateshcoder2003/rockpaper", // Add GitHub link
         imgsrc: "https://th.bing.com/th/id/OIP.bkUn2l2ZimJZYi-Pu9m4mQHaEy?rs=1&pid=ImgDetMain"
     },
+    {
+        id: 3,
+        projectName: "Citizen Science Platform",
+        demo: "",
+        github: "https://citizenscience-1.onrender.com/", // Add GitHub link
+        imgsrc: "https://th.bing.com/th/id/OIP.ucEOAhAvY6yVtTKiTEgZsAHaDO?w=350&h=152&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+    },
 
 ];
 
